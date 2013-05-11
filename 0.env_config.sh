076bf9ea8b2227d5b151b082f39ca8455f45d37b
wu xueqing <wuxqing@gmail.com>
2013-05-11 22:03:42 +0800

    modify python to 2.7.4

--- 0.env_config.sh
+++ 0.env_config.sh
@@ -5,9 +5,11 @@ src_path="$cur_path/source"
 mkdir ${src_path} -p
 
 # 安装开发环境
-yum install gcc gcc-c++ make automake autoconf213 autoconf bison ncurses cmake libtool bison flex perl git subversion mercurial python-setuptools -y
+# python-setuptools 
+yum install gcc gcc-c++ make automake autoconf213 autoconf bison ncurses cmake libtool bison flex perl git subversion mercurial -y
 
 # 安装开发库
-yum install pam-devel openssl-devel pcre-devel zlib-devel libjpeg-devel libtiff-devel libpng-devel freetype-devel boost-devel libevent-devel libuuid-devel readline-devel bzip2-devel libxml2-devel libxslt-devel openssl-devel kernel-devel pcre-devel boost-devel python-devel libpcap-devel ncurses-devel libaio libaio-devel libicu libicu-devel -y
+# python-devel 
+yum install pam-devel openssl-devel pcre-devel zlib-devel libjpeg-devel libtiff-devel libpng-devel freetype-devel boost-devel libevent-devel libuuid-devel readline-devel bzip2-devel libxml2-devel libxslt-devel openssl-devel kernel-devel pcre-devel boost-devel libpcap-devel ncurses-devel libaio libaio-devel libicu libicu-devel -y
 
 mkdir /work/log/ -p
--- 2.inst_python.sh
+++ 2.inst_python.sh
@@ -5,18 +5,21 @@ src_path="$cur_path/source"
 mkdir ${src_path} -p
 
 cd $src_path
-wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2
-tar vxf $src_path/Python-2.7.3.tar.bz2
-cd Python-2.7.3
-./configure --prefix=/opt/python-2.7.3
+wget http://www.python.org/ftp/python/2.7.4/Python-2.7.4.tar.bz2
+tar vxf $src_path/Python-2.7.4.tar.bz2
+cd Python-2.7.4
+./configure --prefix=/opt/python-2.7.4
 make
 make install
 
 cd $src_path
-wget http://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz#md5=7df2a529a074f613b509fb44feefe74e
-tar vxf $src_path/setuptools-0.6c11.tar.gz
-cd setuptools-0.6c11
-/opt/python-2.7.3/bin/python setup.py install
+curl -O http://python-distribute.org/distribute_setup.py
+python distribute_setup.py
+
+#wget http://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz#md5=7df2a529a074f613b509fb44feefe74e
+#tar vxf $src_path/setuptools-0.6c11.tar.gz
+#cd setuptools-0.6c11
+#/opt/python-2.7.3/bin/python setup.py install
 
 #cd $src_path
 #wget http://pypi.python.org/packages/source/v/virtualenvwrapper/virtualenvwrapper-3.6.tar.gz#md5=57d78305b75750a40985f206c80a280f
@@ -26,10 +29,10 @@ cd setuptools-0.6c11
 
 /opt/python-2.7.3/bin/easy_install virtualenvwrapper
 
-mkdir /work
+mkdir /work -p
 
-echo "export VIRTUALENVWRAPPER_PYTHON=/opt/python-2.7.3/bin/python" >> ~/.bashrc
-echo "source /opt/python-2.7.3/bin/virtualenvwrapper.sh" >> ~/.bashrc
+echo "export VIRTUALENVWRAPPER_PYTHON=/opt/python-2.7.4/bin/python" >> ~/.bashrc
+echo "source /opt/python-2.7.4/bin/virtualenvwrapper.sh" >> ~/.bashrc
 echo "export WORKON_HOME=/work/" >> ~/.bashrc
-echo "PATH=\$PATH:/opt/python-2.7.3/bin" >> ~/.bash_profile
+echo "PATH=\$PATH:/opt/python-2.7.4/bin" >> ~/.bash_profile
 echo "export PATH" >> ~/.bash_profile
